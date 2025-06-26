ef9fc1da0e5612c70c97536796831320c5d43336
Jagadishhs69 <157796587+Jagadishhs69@users.noreply.github.com>
2025-06-26 13:17:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from Node.js CI/CD Pipeline!');
+    res.status(200).send('Hello from Node.js');
 });
 
 app.listen(port, () => {
